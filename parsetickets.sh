a135eeab18a1d8612e18558c1416a3e265a9a846
Lucasjt <lucasjuliantoledo@hotmail.com>
2021-10-04 23:44:54 +0200

    check if there is a .pdf in the folder

--- parsetickets.sh
+++ parsetickets.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 # go to tickets folders, convert *.pdf to .txt and delete *.pdf
 cd ~/consum/consum_project/data/tickets_pdf
-for file in *.pdf; do
-  base=${file%.*}
-  pdftotext -layout "$file" "$base.txt"
-  rm $file
-done;
+if [ -e *.pdf ]; then
+  for file in *.pdf; do
+    base=${file%.*}
+    pdftotext -layout "$file" "$base.txt"
+    rm $file
+  done;
+fi
