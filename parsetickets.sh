af31507817a1eeadfb982b62994c2f8a8bfc6728
Lucasjt <lucasjuliantoledo@hotmail.com>
2021-10-19 21:43:20 +0200

    updated none pdf statement

--- parsetickets.sh
+++ parsetickets.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 # go to tickets folders, convert *.pdf to .txt and delete *.pdf
 cd ~/consum/consum_project/data/tickets_pdf
-if [ -e *.pdf ]; then
-  for file in *.pdf; do
-    base=${file%.*}
-    pdftotext -layout "$file" "$base.txt"
-    rm $file
-  done;
-fi
+for file in *.pdf; do
+  if [ ! -e *.pdf ]; then
+    echo "No pdf file"
+    return 0
+  fi
+  base=${file%.*}
+  pdftotext -layout "$file" "$base.txt"
+  rm $file
+done;
